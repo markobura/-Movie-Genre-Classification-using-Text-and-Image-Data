d9cff054b1793b2926c04465bb160edc4daccbf3
Jovana Djurovic <jovana.djurovich@gmail.com>
2026-08-19 22:58:08 +0200

    feat: resize and center crop posters to 224x224

--- src/poster/transforms.py
+++ src/poster/transforms.py
@@ -3,3 +3,10 @@ from torchvision import transforms
 
 def to_tensor_transform():
     return transforms.ToTensor()
+
+
+def resize_center_crop_transform():
+    return transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+    ])
