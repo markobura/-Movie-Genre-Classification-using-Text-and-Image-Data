2986ce546759180e3c2229e575f755c38e0638bc
Jovana Djurovic <jovana.djurovich@gmail.com>
2026-08-21 07:34:41 +0200

    feat: freeze VGG-16 convolutional features for transfer learning

--- src/poster/models.py
+++ src/poster/models.py
@@ -4,3 +4,9 @@ from torchvision import models
 def load_vgg16_imagenet():
     weights = models.VGG16_Weights.IMAGENET1K_V1
     return models.vgg16(weights=weights)
+
+
+def freeze_vgg_features(model):
+    for param in model.features.parameters():
+        param.requires_grad = False
+    return model
