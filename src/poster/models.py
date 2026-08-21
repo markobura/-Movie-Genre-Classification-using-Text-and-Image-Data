9d13f73cf9324ef10343c1964e26ea5eb5c73764
Jovana Djurovic <jovana.djurovich@gmail.com>
2026-08-21 07:58:33 +0200

    feat: replace VGG classifier head for 13 genre lables

--- src/poster/models.py
+++ src/poster/models.py
@@ -1,4 +1,7 @@
+import torch.nn as nn
 from torchvision import models
+
+from .config import GENRES
 
 
 def load_vgg16_imagenet():
@@ -10,3 +13,16 @@ def freeze_vgg_features(model):
     for param in model.features.parameters():
         param.requires_grad = False
     return model
+
+
+def replace_vgg_classifier(model, num_classes=len(GENRES)):
+    in_features = model.classifier[0].in_features
+    model.classifier = nn.Sequential(
+        nn.Linear(in_features, 4096),
+        nn.ReLU(True),
+        nn.Linear(4096, 1000),
+        nn.ReLU(True),
+        nn.Linear(1000, num_classes),
+    )
+    return model
+
